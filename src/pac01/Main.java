469668b4ea52b37bcd5fc656c282ae7dfb81bd43
ryosuke0124 <ryosuke19910124@gmail.com>
2019-11-28 09:30:06 +0000

    Wizardクラスを生成して回復魔法を使えるようにしました

--- src/pac01/Main.java
+++ src/pac01/Main.java
@@ -10,5 +10,20 @@ public class Main {
 		h.hp = 100;
 		h.sword = s;
 		System.out.println("現在の武器は" + h.sword.name);
+		
+		Hero h1 = new Hero();
+		h1.name = "ミナト";
+		h1.hp = 100;
+		
+		Hero h2 = new Hero();
+		h2.name = "アサカ";
+		h2.hp = 100;
+		
+		Wizard w = new Wizard();
+		w.name = "スガワラ";
+		w.hp = 50;
+		w.heal(h1);
+		w.heal(h2);
+		w.heal(h2);
 	}
 }
